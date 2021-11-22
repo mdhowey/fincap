d0c2b7cf8e40092de05b6a19c4ea57bd6647de81
devhowey <dev.howey@gmail.com>
2021-11-22 12:37:14 -0600

    set up index.js in models

---
+++ api/models/index.js
@@ -0,0 +1,7 @@
+model.exports = {
+  User: require('./User'),
+  Account: require('./Account'),
+  Income: require('./Income'),
+  Expense: require('./Expense'),
+  Transfer: require('./Transfer'),
+};
