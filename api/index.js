b14e0a258ca1efe2a4678ec881eb5aaa6e6850e1
devhowey <dev.howey@gmail.com>
2021-11-22 12:50:57 -0600

    required in routes to index,js

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 PORT = 3000;
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const routes = require('./routes');
 
 dotenv.config();
 
@@ -12,6 +13,8 @@ mongoose.connect(process.env.MONGO_URL, {
 })
   .then(console.log('Connected to MongoDB'))
   .catch(err => console.log(`========== Connection issue with MongoDB ========== ${err}`));
+
+// app.use('/', routes.transfer)
 
 app.listen(PORT, () => {
   console.log(`API is running on port on ${PORT}`);
