10267d58754a9e64ce542d324efbb92b005e6d30
devhowey <dev.howey@gmail.com>
2021-11-23 16:59:58 -0600

    wasting time on styling

--- client/src/components/Balance/Balance.jsx
+++ client/src/components/Balance/Balance.jsx
@@ -3,9 +3,23 @@ import classes from './Balance.module.scss';
 
 export default function Balance() {
   return (
-    <div>
-      <h4 >Current Balance</h4>
-      <h2>$0.00</h2>
+    <div className={classes.balance}>
+      <div className={classes.balance__acct}>
+        <h4 className={classes.balance__acct__head}>Account</h4>
+        <h4 className={classes.balance__acct__head}>Balance</h4>
+      </div>
+      <div className={classes.balance__acct}>
+        <h4 className={classes.balance__acct__title}>Savings</h4>
+        <h4 className={classes.balance__acct__bal}>$0.00</h4>
+      </div>
+      <div className={classes.balance__acct}>
+        <h4 className={classes.balance__acct__title}>Savings</h4>
+        <h4 className={classes.balance__acct__bal}>$0.00</h4>
+      </div>
+      <div className={classes.balance__acct}>
+        <h4 className={classes.balance__acct__title}>Savings</h4>
+        <h4 className={classes.balance__acct__bal}>$0.00</h4>
+      </div>
     </div>
   )
 }
