946ecc8548604ea0665625e81631a1d7f38295a4
devhowey <dev.howey@gmail.com>
2021-11-24 15:27:37 -0600

    added date to activity section

--- client/src/components/TransactionList.jsx/TransactionList.jsx
+++ client/src/components/TransactionList.jsx/TransactionList.jsx
@@ -6,9 +6,15 @@ import Transaction from '../Transaction/Transaction';
 export default function TransactionList() {
   const { transactions } = useContext(Context);
 
+  const current = new Date();
+  const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+
   return (
     <div className={classes.transList}>
-      <h4 className={classes.transList__head}>Activity</h4>
+      <div className={classes.transList__cont}>
+        <h4 className={classes.transList__cont__head}>Activity</h4>
+        <h4 className={classes.transList__cont__date}>{date}</h4>
+      </div>
       <ul className={classes.transList__list}>
         {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
       </ul>
